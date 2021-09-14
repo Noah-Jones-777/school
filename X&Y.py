e17205b52ce93a3c76f6f00d913c0f422db5c97d
Noah Jones <90513501+Noah-Jones-777@users.noreply.github.com>
2021-09-14 08:41:36 -0500

    Add files via upload

---
+++ X&Y.py
@@ -0,0 +1,33 @@
+######################################################################################################################
+# Name: Noah Jones
+# Date: 9/13/2021
+# Description: program to do simple X & Y coordinate calculations such as midpoint and distance between two points
+######################################################################################################################
+
+# the 2D point class
+class Point:
+	def 
+	# write your code for the point class here (and subsequently remove this comment)
+
+##########################################################
+# ***DO NOT MODIFY OR REMOVE ANYTHING BELOW THIS POINT!***
+# create some points
+p1 = Point()
+p2 = Point(3, 0)
+p3 = Point(3, 4)
+# display them
+print("p1:", p1)
+print("p2:", p2)
+print("p3:", p3)
+# calculate and display some distances
+print("distance from p1 to p2:", p1.dist(p2))
+print("distance from p2 to p3:", p2.dist(p3))
+print("distance from p1 to p3:", p1.dist(p3))
+# calculate and display some midpoints
+print("midpt of p1 and p2:", p1.midpt(p2))
+print("midpt of p2 and p3:", p2.midpt(p3))
+print("midpt of p1 and p3:", p1.midpt(p3))
+# just a few more things...
+p4 = p1.midpt(p3)
+print("p4:", p4)
+print("distance from p4 to p1:", p4.dist(p1))
