6097f212a52ae921dbc2cb395d810862539ca726
Noah Jones <90513501+Noah-Jones-777@users.noreply.github.com>
2021-09-14 10:16:20 -0500

    Add files via upload

--- X&Y.py
+++ X&Y.py
@@ -4,10 +4,77 @@
 # Description: program to do simple X & Y coordinate calculations such as midpoint and distance between two points
 ######################################################################################################################
 
+#importing math for program calculations
+import math
+
 # the 2D point class
 class Point:
-	def 
-	# write your code for the point class here (and subsequently remove this comment)
+	
+	#class constructor with default values set at 0
+	def __init__(self, x=0, y=0):
+		self.x = x 
+		self.y = y
+
+		
+	#acessor
+	@property
+	def x(self):
+		return self._x
+
+	#mutator
+	@x.setter
+	def x(self, value):
+		self._x = value
+
+	#acessor
+	@property
+	def y(self):
+		return self._y
+
+	#mutator
+	@y.setter
+	def y(self, value):
+		self._y = value
+		
+
+	"""#function to retrive apporaiate coordinates in the needed mathematical expression
+	def Coordinate_Retriver(self, set1, set2):
+		x1, x2 = set1.x, set2.x
+		y1, y2 = set1.y, set2.y
+		return x1, y1, x2, y2"""
+
+
+	#class method to calculate midpoint
+	def midpt(set1, set2):
+	
+		x1, x2 = set1.x, set2.x
+		y1, y2 = set1.y, set2.y
+
+		#algorithm for the midpoint coordinates
+		midpointX, midpointY = (((x1 + x2) / 2), ((y1 + y2) / 2))
+		return(midpointX, midpointY)
+
+
+	#class method to calculate distance between two points
+	def dist(set1, set2):
+
+		x1, x2 = set1.x, set2.x
+		y1, y2 = set1.y, set2.y
+
+		#algorithm for distance between two points
+		partx = ((x2-x1)**2)
+		party = ((y2-y1)**2)
+		to_be_squared = partx + party
+		distance = math.sqrt(to_be_squared)
+		return ("{}".format(distance))
+
+	#string
+	def __str__(self):
+		return(" ({}, {})".format(self.x, self.y))
+
+
+
+
 
 ##########################################################
 # ***DO NOT MODIFY OR REMOVE ANYTHING BELOW THIS POINT!***
