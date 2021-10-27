9825e411d215598cdafa6603b35170e4ccf535f6
Noah Jones <90513501+Noah-Jones-777@users.noreply.github.com>
2021-10-27 13:21:12 -0500

    minor updates

--- ChaosReloded.py
+++ ChaosReloded.py
@@ -7,16 +7,11 @@
 #importing necessary libraries
 from tkinter import *
 import random
-from Fractals import Fractal
-from point import Point
-
-
-
-
+from Fractals import *
 
 #class holding our canvas and necessary functions to plot the sierpinski triangle
 class ChaosGame(Canvas):
-	def __init__(self, window):
+	def __init__(self, window, plotcolor):
 		self.window = window
 		self.dimensions = {
 		"width":600,
@@ -30,115 +25,59 @@ class ChaosGame(Canvas):
 		}
 		self.vertexradius = 2
 		self.vertexcolor = "red"
-		self.pointcolor = "black"
+		self.plotcolor = str(plotcolor)
 
 	def make(self, shape):
 		C = Canvas(self.window, bg = "white", height=self.dimensions["height"], width=self.dimensions["width"])
-		F = Fractal(C, shape, self.dimensions)
+		#using the eval keyworkd since the fractal subclass constructor will be a string
+		parameters = "{}(C, self.dimensions)".format(shape)
+		F = eval(parameters)
 		self.plotpoints(F, C)
+	
 
 	def plotpoints(self, fractal_obj, canvas_obj):
 		for i in fractal_obj.Vs:
-			canvas_obj.create_oval(i.x, i.y, i.x+2, i.y+2, outline="red", fill="red")
-		canvas_obj.pack()
+			canvas_obj.create_oval(i.x, i.y, i.x+self.vertexradius, i.y+self.vertexradius,\
+			 outline=self.vertexcolor, fill=self.vertexcolor)
+		#number of points to plot
 		limit = fractal_obj.numpoints
-		v1 = random.choice(fractal_obj.Vs) 
-		v2 = random.choice(fractal_obj.Vs)
+		#our first two randomly selected verticies
+		base_points = fractal_obj.Vs
+		v1, v2 = random.choices(base_points, k = 2)
 		temporary_point = v1.midpt(v2)
-		canvas_obj.create_oval(temporary_point.x, temporary_point.y, temporary_point.x, temporary_point.y, outline="black", fill="black")
+		canvas_obj.create_oval(temporary_point.x, temporary_point.y, temporary_point.x, temporary_point.y,\
+		 outline="black", fill="black")
+		#algorithm for plotting points using everything we've combined
 		while limit > 0:
 			random_v = random.choice(fractal_obj.Vs)
-			new_point = temporary_point.midpt(random_v)
-			canvas_obj.create_oval(new_point.x, new_point.y, new_point.x, new_point.y, outline="black", fill="black")
+			new_point = temporary_point.interpt(random_v, fractal_obj.r)
+			#plotting our point
+			canvas_obj.create_oval(new_point.x, new_point.y, new_point.x, new_point.y,\
+				outline=self.plotcolor, fill=self.plotcolor)
 			temporary_point = new_point
 			limit-=1
 		canvas_obj.pack()
 
 
-
-
-#test stuff
+##############
+#MAIN PROGRAM#
+##############
 # the default size of the canvas is 600x520
 WIDTH = 600
 HEIGHT = 520
-
-window = Tk()
-window.geometry("{}x{}".format(WIDTH, HEIGHT))
-window.title("The Chaos Game...Reloaded")
-# create the game as a Tkinter canvas inside the window
-s = ChaosGame(window)
-# make the current fractal
-s.make("Hexagon")
-# wait for the window to close
-window.mainloop()
-
-
-
-
-
-"""
-	#function to plot colored points on the Tkinter canvas
-	def plotPoints(self, limit):
-	
-		#setting our canvas to the original tkinter window and giving it client specified parameters
-		C = Canvas(self.window, bg="white", height = 520, width = 600)
-		
-		#creating verticies objects to hold our base plot points
-		v1= Point(294, 2)
-		v2= Point(5, 510)
-		v3= Point(595, 510)
-		C.create_oval(v1.x, v1.y, v1.x+5, v1.y+5, outline="red", fill="red")
-		C.create_oval(v2.x, v2.y, v2.x+5, v2.y-5, outline="red", fill="red")
-		C.create_oval(v3.x, v3.y, v3.x-5, v3.y-5, outline="red", fill="red")
-		C.pack()
-
-
-		#getting our new midpoint
-		basepoints = [v1, v2, v3]
-		basepoint1, basepoint2 = random.choices(basepoints, k = 2)
-		NewPlot= basepoint1.midpt(basepoint2)
-		C.create_oval(NewPlot.x, NewPlot.y, NewPlot.x, NewPlot.y, outline="black", fill="black")
-		#packing our pixel to the tkinter string
-		C.pack()
-
-		#while loop to plot number of points(limit)
-		while limit > 0:
-			
-			#ramdom verticie picked from list
-			V = random.choice(basepoints)
-
-			#The newnewplot is the midpoint of a random verticie and our previously created new point otherwise knonw as NewPlot
-			NewNewPlot= V.midpt(NewPlot)
-
-			#Setting the new midpoint as the base point to be used in the midpoint formula with the randomly selected verticie 
-			NewPlot = NewNewPlot
-			C.create_oval(NewNewPlot.x, NewNewPlot.y, NewNewPlot.x, NewNewPlot.y, outline="black", fill="black")
-			#packing our pixel to the tkinter string
-			C.pack()
-			#taking away number of points to be plotted 
-			limit -= 1
-			"""
-	
-
-
-
-
-
-
-"""
-##############
-#MAIN PROGRAM
-##############
-#window parameters
-WIDTH = 800
-HEIGHT = 800
-NUM_POINTS = 50000
-# create the window
-window = Tk()
-window.geometry("{}x{}".format(WIDTH, HEIGHT))
-window.title("THE CHAOS GAME")
-# create the coordinate system as a Tkinter canvas inside the window
-s = Chaos(window)
-s.plotPoints(NUM_POINTS)
-# wait for the window to close
-window.mainloop()"""
+#colors for fun
+#excluding red to see verticies
+colors = ["red", "blue", "black", "green", "pink", "orange", "yellow", "purple", "gray", "turquoise"]
+Fractals = ["SierpinskiTriangle", "SierpinskiCarpet",\
+"Pentagon", "Hexagon", "Octagon"]
+for f in Fractals:
+	random_color = random.choice(colors)
+	window = Tk()
+	window.geometry("{}x{}".format(WIDTH, HEIGHT))
+	window.title("The Chaos Game...Reloaded")
+	# create the game as a Tkinter canvas inside the window
+	s = ChaosGame(window, random_color)
+	# make the current fractal
+	s.make(f)
+	# wait for the window to close
+	window.mainloop()
--- Fractals.py
+++ Fractals.py
@@ -1,106 +1,182 @@
 ######################################################################################################################
 # Name: Noah Jones
 # Date: 10/25/21
-# Description: choas game with additional shapes
+# Description: Fractal and Point class with additional Fractal sublclasses
 ######################################################################################################################
-from point import Point
 from tkinter import *
 import math
 
 
+# the 2D point class
+class Point:
+	
+	#class constructor with default values set at 0
+	def __init__(self, x=0, y=0):
+		self.x = float(x) 
+		self.y = float(y)
+
+	#acessor
+	@property
+	def x(self):
+		return self._x
+
+	#mutator
+	@x.setter
+	def x(self, value):
+		self._x = value
+
+	#acessor
+	@property
+	def y(self):
+		return self._y
+
+	#mutator
+	@y.setter
+	def y(self, value):
+		self._y = value
+		
+	#class method to calculate midpoint
+	def midpt(set1, set2):
+	
+		#get necessary point data for calculations
+		x1, x2 = set1.x, set2.x
+		y1, y2 = set1.y, set2.y
+
+		#algorithm for the midpoint coordinates
+		midpointX, midpointY = (((x1 + x2) / 2), ((y1 + y2) / 2))
+
+		#creating new Point object to in order for new object to use further distance method
+		new_obj = Point(midpointX, midpointY)
+		return new_obj
+
+	#class method to calculate distance between two points
+	def dist(set1, set2):
+
+		#get necessary point data for calculations
+		x1, x2 = set1.x, set2.x
+		y1, y2 = set1.y, set2.y
+
+		#algorithm for distance between two points
+		partx = ((x2-x1)**2)
+		party = ((y2-y1)**2)
+		to_be_squared = partx + party
+		distance = math.sqrt(to_be_squared)
+		return distance
+
+	def interpt(self, other, r):
+		#the x-component
+		rx = r 
+		if (self.x > other.x):
+			rx = 1.0 - r 
+		#the y-component
+		ry = r
+		if (self.y > other.y):
+			ry = 1.0 - r 
+
+		x = abs(self.x-other.x)* rx + min(self.x, other.x)
+		y = abs(self.y - other.y) * ry + min(self.y, other.y)
+		return Point(x,y)
+
+	#magic function to dictate how the object will be printed out
+	def __str__(self):
+		return(" ({},{})".format(self.x, self.y))
+
+
+#The Fractal class
 class Fractal(Point, Canvas):
-	def __init__(self, canvas, shape, dimensions):
-		self.shape = shape
+	def __init__(self, canvas, dimensions):
+		#dimensions is the dictionary provided by the chaos game
 		self.dimensions = dimensions
 		self.canvas = canvas
 
-		def frac_x(r):
-			return int((self.dimensions["max-x"] - \
-				self.dimensions["min-x"]) * r) + \
-				self.dimensions["min-x"]
+	def frac_x(self, r):
+		return int((self.dimensions["max-x"] - \
+			self.dimensions["min-x"]) * r) + \
+			self.dimensions["min-x"]
 
-		def frac_y(r):
-			return int((self.dimensions["max-y"] - \
-				self.dimensions["min-y"]) * r) + \
-				self.dimensions["min-y"]
+	def frac_y(self, r):
+		return int((self.dimensions["max-y"] - \
+			self.dimensions["min-y"]) * r) + \
+			self.dimensions["min-y"]
 
-		if self.shape == "SierpinskiTriangle":
-			self.r = 0.5
-			v1 = Point(self.dimensions["mid-x"], self.dimensions["min-y"])
-			v2 = Point(self.dimensions["min-x"], self.dimensions["max-y"])
-			v3 = Point(self.dimensions["max-x"], self.dimensions["max-y"])
-			self.Vs = [v1, v2, v3]
-			self.numpoints=50000
+#Triangle Fractal sublcass
+class SierpinskiTriangle(Fractal):
+	def __init__(self, canvas, dimensions):
+		#using Fractal __init__ treatment of canvas and dimensions
+		super().__init__(canvas, dimensions)
+		self.r = 0.5
+		v1 = Point(self.dimensions["mid-x"], self.dimensions["min-y"])
+		v2 = Point(self.dimensions["min-x"], self.dimensions["max-y"])
+		v3 = Point(self.dimensions["max-x"], self.dimensions["max-y"])
+		self.Vs = [v1, v2, v3]
+		self.numpoints=50000
 
-		if self.shape == "Hexagon":
-			self.r = 0.665
-			v1 = Point(self.dimensions["min-x"], self.dimensions["height"]*.5)
-			v2 = Point(self.dimensions["width"]*.25, self.dimensions["min-y"])
-			v3 = Point(self.dimensions["width"]*.75, self.dimensions["min-y"])
-			v4 = Point(self.dimensions["max-x"], self.dimensions["height"]*.5)
-			v5 = Point(self.dimensions["width"]*.25, self.dimensions["max-y"])
-			v6 = Point(self.dimensions["width"]*.75, self.dimensions["max-y"])
-			self.Vs = [v1,v2,v3,v4,v5,v6]
-			self.numpoints=50000
+#Hexagon Fractal sublclass
+class Hexagon(Fractal):
+	def __init__(self, canvas, dimensions):
+		super().__init__(canvas, dimensions)
+		self.r = 0.665
+		v1 = Point(self.dimensions["mid-x"], self.dimensions["min-y"])
+		v2 = Point(self.dimensions["min-x"], self.frac_y(0.25))
+		v3 = Point(self.dimensions["max-x"], self.frac_y(0.25))
+		v4 = Point(self.dimensions["min-x"], self.frac_y(0.75))
+		v5 = Point(self.dimensions["max-x"], self.frac_y(0.75))
+		v6 = Point(self.dimensions["mid-x"], self.dimensions["max-y"])
+		self.Vs = [v1,v2,v3,v4,v5,v6]
+		self.numpoints=50000
 		
-		if self.shape == "Pentagon":
-			self.r = 0.618
-			v1 = Point(self.dimensions["mid-x"]*2*cos(2*pi/5+60), (frac_y(0.5375)+self.dimensions["mid-y"]*sin(2*pi/5+60)))
-			v2 = Point(self.dimensions["mid-x"]*2*cos(4*pi/5+60), (frac_y(0.5375)+self.dimensions["mid-y"]*sin(4*pi/5+60)))
-			v3 = Point(self.dimensions["mid-x"]*2*cos(6*pi/5+60), (frac_y(0.5375)+self.dimensions["mid-y"]*sin(6*pi/5+60)))
-			v4 = Point(self.dimensions["mid-x"]*2*cos(8*pi/5+60), (frac_y(0.5375)+self.dimensions["mid-y"]*sin(8*pi/5+60)))
-			v5 = Point(self.dimensions["mid-x"]*2*cos(10*pi/5+60), (frac_y(0.5375)+self.dimensions["mid-y"]*sin(10*pi/5+60)))
-			self.Vs=[v1,v2,v3,v4,v5]
-			self.numpoints=50000
+#Pentagon Fractal subclass
+class Pentagon(Fractal):
+	def __init__(self, canvas, dimensions):
+		super().__init__(canvas, dimensions)
+		self.r = 0.618
+		#This a variable to hold mid-x + mid-x so the statement is not as large
+		Dmidx = self.dimensions["mid-x"] * 2
+		v1 = Point(Dmidx*math.cos(2*math.pi/5+60),\
+		 (self.frac_y(0.5375)+self.dimensions["mid-y"]*math.sin(2*math.pi/5+60)))
 
-		if self.shape == "SierpinskiCarpet":
-			self.r = 0.66
-			v1=Point(self.dimensions["min-x"], self.dimensions["min-y"])
-			v2=Point(self.dimensions["mid-x"], self.dimensions["min-y"])
-			v3=Point(self.dimensions["max-x"], self.dimensions["min-y"])
-			v4=Point(self.dimensions["min-x"], self.dimensions["mid-y"])
-			v5=Point(self.dimensions["max-x"], self.dimensions["mid-y"])
-			v6=Point(self.dimensions["min-x"], self.dimensions["max-y"])
-			v7=Point(self.dimensions["mid-x"], self.dimensions["max-y"])
-			v8=Point(self.dimensions["max-x"], self.dimensions["max-y"])
-			self.numpoints=100000
-			self.Vs=[v1,v2,v3,v4,v5,v6,v7,v8]
+		v2 = Point(Dmidx*math.cos(4*math.pi/5+60),\
+		 (self.frac_y(0.5375)+self.dimensions["mid-y"]*math.sin(4*math.pi/5+60)))
 
-		if self.shape == "Octagon":
-			self.r =0.705
-			v1=Point(frac_x(0.2925),self.dimensions["min-y"])
-			v2=Point(frac_x(0.7075),self.dimensions["min-y"])
-			v3=Point(self.dimensions["min-x"], frac_y(0.2925))
-			v4=Point(self.dimensions["max-x"], frac_y(0.2925))
-			v5=Point(self.dimensions["min-x"], frac_y(0.7075))
-			v6=Point(self.dimensions["max-x"], frac_y(0.7075))
-			v7=Point(frac_x(0.2925), self.dimensions["max-y"])
-			v8=Point(frac_x(0.7075), self.dimensions["max-y"])
-			self.numpoints=75000
-			self.Vs=[v1,v2,v3,v4,v5,v6,v7,v8]
-			
+		v3 = Point(Dmidx*math.cos(6*math.pi/5+60),\
+		 (self.frac_y(0.5375)+self.dimensions["mid-y"]*math.sin(6*math.pi/5+60)))
 
-#############
-#MAIN PROGRAM
-#############
-"""
-# the default size of the canvas is 600x520
-WIDTH = 600
-HEIGHT = 520
-# the implemented fractals
-FRACTALS = [ "SierpinskiTriangle", "SierpinskiCarpet",\
+		v4 = Point(Dmidx*math.cos(8*math.pi/5+60),\
+		 (self.frac_y(0.5375)+self.dimensions["mid-y"]*math.sin(8*math.pi/5+60)))
 
-"Pentagon", "Hexagon", "Octagon" ]
+		v5 = Point(Dmidx*math.cos(10*math.pi/5+60),\
+		 (self.frac_y(0.5375)+self.dimensions["mid-y"]*math.sin(10*math.pi/5+60)))
+		self.Vs=[v1,v2,v3,v4,v5]
+		self.numpoints=50000
 
-# create the fractals in individual (sequential) windows
-for f in FRACTALS:
-	window = Tk()
-	window.geometry("{}x{}".format(WIDTH, HEIGHT))
-	window.title("The Chaos Game...Reloaded")
-	# create the game as a Tkinter canvas inside the window
-	s = ChaosGame(window)
-	# make the current fractal
-	s.make(f)
-	# wait for the window to close
-	window.mainloop()"""
+#Square Fractal sublcass
+class SierpinskiCarpet(Fractal):
+	def __init__(self, canvas, dimensions):
+		super().__init__(canvas, dimensions)
+		self.r = 0.66
+		v1=Point(self.dimensions["min-x"], self.dimensions["min-y"])
+		v2=Point(self.dimensions["mid-x"], self.dimensions["min-y"])
+		v3=Point(self.dimensions["max-x"], self.dimensions["min-y"])
+		v4=Point(self.dimensions["min-x"], self.dimensions["mid-y"])
+		v5=Point(self.dimensions["max-x"], self.dimensions["mid-y"])
+		v6=Point(self.dimensions["min-x"], self.dimensions["max-y"])
+		v7=Point(self.dimensions["mid-x"], self.dimensions["max-y"])
+		v8=Point(self.dimensions["max-x"], self.dimensions["max-y"])
+		self.numpoints=100000
+		self.Vs=[v1,v2,v3,v4,v5,v6,v7,v8]
 
+#Octagon Fractal subclass
+class Octagon(Fractal):
+	def __init__(self, canvas, dimensions):
+		super().__init__(canvas, dimensions)
+		self.r =0.705
+		v1=Point(self.frac_x(0.2925),self.dimensions["min-y"])
+		v2=Point(self.frac_x(0.7075),self.dimensions["min-y"])
+		v3=Point(self.dimensions["min-x"], self.frac_y(0.2925))
+		v4=Point(self.dimensions["max-x"], self.frac_y(0.2925))
+		v5=Point(self.dimensions["min-x"], self.frac_y(0.7075))
+		v6=Point(self.dimensions["max-x"], self.frac_y(0.7075))
+		v7=Point(self.frac_x(0.2925), self.dimensions["max-y"])
+		v8=Point(self.frac_x(0.7075), self.dimensions["max-y"])
+		self.numpoints=75000
+		self.Vs=[v1,v2,v3,v4,v5,v6,v7,v8]
